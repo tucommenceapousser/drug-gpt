f87b634c279243822c70acaad9e50ff2f776c6a1
trhacknon <129875733+tucommenceapousser@users.noreply.github.com>
2025-02-11 01:38:39 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from agent import generate_stream, generate_response
 
 
 st.set_page_config(
-    page_title="Drug-GPT",
+    page_title="Trkn-Drug-GPT",
     page_icon="💊",
     menu_items={
         'About': "This app is a prototype made for demo purposes. It is not meant for real life use. Do not take anything from this app as advice."
@@ -24,7 +24,7 @@ if "messages" not in st.session_state:
 # if len(st.session_state.messages) > 1:
 for message in st.session_state.messages:
     if message["role"] == "assistant":     
-        with st.chat_message(name="assistant", avatar="👨‍⚕️"):
+        with st.chat_message(name="assistant", avatar="😈"):
             st.markdown(message["content"])
     else:
         with st.chat_message(message["role"]):
@@ -39,7 +39,7 @@ if prompt := st.chat_input("Message Drug-GPT"):
         st.markdown(prompt)
 
 # Display assistant response in chat message container
-    with st.chat_message(name="assistant", avatar="👨‍⚕️"):
+    with st.chat_message(name="assistant", avatar="😈"):
         stream = generate_stream(st.session_state.messages)
         response = st.write_stream(stream)
     st.session_state.messages.append({"role": "assistant", "content": response})
